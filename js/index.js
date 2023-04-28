eff5cec067fc72a9465d319248dc25e9efef7dec
proyecto246 <proyetocca123@gmail.com>
2023-04-27 23:48:03 -0600

    Adición de animaciones v.2

---
+++ js/index.js
@@ -0,0 +1,31 @@
+window.sr =ScrollReveal();
+
+    sr.reveal('.carousel',{
+        duration: 3000,
+        origin: 'botton',
+        distance: '-100px'
+    });
+    
+
+    sr.reveal('.presentacion',{
+        duration: 2500,
+        origin: 'botton',
+        distance: '100px'
+    });
+
+    sr.reveal('.historia',{
+        duration: 2500,
+        origin: 'botton',
+        distance: '-100px'
+    });
+
+    sr.reveal('.proyectos',{
+        duration: 2500,
+        origin: 'botton',
+        distance: '100px'
+    });
+
+    sr.reveal('.videos',{
+        duration: 2500,
+        
+    });
